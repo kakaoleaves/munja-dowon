d35ebe023ee17a84c63e727140d2de7beeb61794
kakaoleaves <kakaoleaves@naver.com>
2022-11-22 00:45:01 +0900

    add homepage'

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   
 
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path='/' element={<Layout />}>
                     <Route path='/home' element={<Home />} />
